17578b562d859adc3d679bd18a10de1b090b0aee
Kevin Budd <kevin@kevinbudd.com>
2018-07-08 21:40:57 -0500

    assignment completed

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,6 +2,12 @@ require 'sqlite3'
 
 module Selection
   def find(*ids)
+    ids.each do |id|
+      unless id.is_a? Integer && id >= 0
+        raise ArgumentError.new('Invalid ID.')
+      end
+    end
+
     if ids.length == 1
       find_one(ids.first)
     else
@@ -15,6 +21,10 @@ module Selection
   end
 
   def find_one(id)
+    unless id.is_a? Integer && id >= 0
+      raise ArgumentError.new('Invalid ID.')
+    end
+
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id};
@@ -23,6 +33,10 @@ module Selection
   end
 
   def find_by(attribute, value)
+    unless columns.include?(attribute)
+      raise ArgumentError.new('Invalid attribute.')
+    end
+
     rows = connection.execute <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
@@ -69,6 +83,39 @@ module Selection
     SQL
 
     rows_to_array(rows)
+  end
+
+  def find_each(start=0, batch_size=nil)
+    if batch_size
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size}
+        OFFSET #{start};
+      SQL
+
+      rows_to_array(rows).each do |each|
+        yield
+      end
+    else
+      all.each do |each|
+        yield
+      end
+    end
+  end
+
+  def find_in_batches(start=0, batch_size=nil)
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size}
+      OFFSET #{start};
+    SQL
+
+    yield rows_to_array(rows)
+  end
+
+  def method_missing(m, *args, &block)
+    attribute = m.to_s.sub('find_by_', '').to_sym
+    find_by(attribute, *args[0])
   end
 
   private
